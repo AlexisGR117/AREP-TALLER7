70313a776770b16283d5b206ed759a2abc42e4f9
AlexisGR117 <jefer12543@gmail.com>
2024-03-17 12:19:08 -0500

    Con conexión segura

--- src/main/java/edu/eci/arep/LoginService.java
+++ src/main/java/edu/eci/arep/LoginService.java
@@ -1,34 +1,123 @@
 package edu.eci.arep;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static spark.Spark.*;
 
+/**
+ * Clase que implementa un servicio de inicio de sesión utilizando un servidor web.
+ * El servicio de inicio de sesión utiliza un servidor web para manejar las solicitudes de inicio de sesión.
+ * El servicio se configura para escuchar en el puerto especificado en la variable de entorno "PORT" o en el puerto 5000 si no se especifica.
+ * El servicio utiliza un certificado para garantizar la seguridad de las comunicaciones.
+ * El servicio de inicio de sesión expone un único punto final "/login" que acepta dos parámetros: "name" y "password".
+ *
+ * @author Jefer Alexis Gonzalez Romero
+ * @version 1.0 (17/03/2023)
+ */
 public class LoginService {
 
-    private static Usuarios usuarios;
-
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        usuarios = new Usuarios();
+    /**
+     * Método principal que inicia el servicio de inicio de sesión.
+     *
+     * @param args Argumentos de la línea de comandos.
+     * @throws NoSuchAlgorithmException Si no se encuentra el algoritmo de seguridad.
+     * @throws KeyStoreException        Si ocurre un error al acceder al almacén de claves.
+     * @throws IOException              Si ocurre un error al leer o escribir datos.
+     * @throws KeyManagementException   Si ocurre un error al gestionar claves.
+     * @throws CertificateException     Si ocurre un error al cargar certificados.
+     */
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException, CertificateException {
         staticFiles.location("/public");
         port(getPort());
         secure("certificados/ecikeystore.p12", "123456", null, null);
-        get("/hello", (req, res) -> "Hello World");
+        configureTrustedSSLContext();
         get("/login", (req, res) -> {
             res.type("application/json");
-            return loginResult(req.queryParams("name"), req.queryParams("password"));
+            return readURL("name=" + req.queryParams("name") + "&password=" + req.queryParams("password"));
         });
     }
 
-    public static String loginResult(String name, String password) throws NoSuchAlgorithmException {
-        boolean result = usuarios.verifyPassword(name, password);
-        return "{\"result\":" + result + "}";
-    }
-
+    /**
+     * Obtiene el puerto en el que el servidor debe escuchar.
+     * Si la variable de entorno "PORT" está definida, se devuelve el valor de la variable de entorno como un entero.
+     * De lo contrario, se devuelve el valor predeterminado 5000.
+     *
+     * @return El puerto en el que el servidor debe escuchar.
+     */
     public static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    /**
+     * Configura el contexto SSL predeterminado con un almacén de claves de confianza.
+     * Este método crea un almacén de claves de confianza a partir del archivo "certificados/myTrustStore.p12" con la contraseña "123456".
+     * Luego, crea un contexto SSL utilizando el almacén de claves de confianza y lo establece como el contexto SSL predeterminado.
+     *
+     * @throws KeyStoreException        Si ocurre un error al acceder al almacén de claves.
+     * @throws IOException              Si ocurre un error al leer o escribir datos.
+     * @throws NoSuchAlgorithmException Si no se encuentra el algoritmo de seguridad.
+     * @throws KeyManagementException   Si ocurre un error al gestionar claves.
+     * @throws CertificateException     Si ocurre un error al cargar certificados.
+     */
+    private static void configureTrustedSSLContext() throws KeyStoreException, IOException, NoSuchAlgorithmException, KeyManagementException, CertificateException {
+        File trustStoreFile = new File("certificados/myTrustStore.p12");
+        char[] trustStorePassword = "123456".toCharArray();
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        trustStore.load(new FileInputStream(trustStoreFile), trustStorePassword);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(trustStore);
+        for (TrustManager t : tmf.getTrustManagers()) System.out.println(t);
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+        SSLContext.setDefault(sslContext);
+    }
+
+    /**
+     * Lee el contenido de una URL y devuelve la respuesta como una cadena.
+     * Este método construye una URL a partir de la cadena de consulta proporcionada y la URL base estática.
+     * Luego, abre una conexión a la URL y obtiene los encabezados de la respuesta.
+     * Por último, lee el cuerpo de la respuesta y lo devuelve como una cadena.
+     *
+     * @param query La cadena de consulta para construir la URL.
+     * @return La respuesta de la URL como una cadena.
+     * @throws IOException Si ocurre un error al leer o escribir datos.
+     */
+    public static String readURL(String query) throws IOException {
+        URL siteURL = new URL("https://localhost:5002/user?" + query);
+        URLConnection urlConnection = siteURL.openConnection();
+        Map<String, List<String>> headers = urlConnection.getHeaderFields();
+        Set<Map.Entry<String, List<String>>> entrySet = headers.entrySet();
+        for (Map.Entry<String, List<String>> entry : entrySet) {
+            String headerName = entry.getKey();
+            if (headerName != null) System.out.print(headerName + ":");
+            List<String> headerValues = entry.getValue();
+            for (String value : headerValues) System.out.print(value);
+            System.out.println("");
+        }
+        System.out.println("-------message-body------");
+        StringBuffer response = new StringBuffer();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        String inputLine;
+        while ((inputLine = reader.readLine()) != null) response.append(inputLine);
+        reader.close();
+        System.out.println(response);
+        System.out.println("GET DONE");
+        return response.toString();
     }
 }
---
+++ src/main/java/edu/eci/arep/UsersService.java
@@ -0,0 +1,94 @@
+package edu.eci.arep;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static spark.Spark.*;
+
+/**
+ * Clase que gestiona los usuarios y sus contraseñas.
+ * La clase almacena los usuarios y sus contraseñas hash en un mapa estático.
+ * La clase proporciona métodos para agregar usuarios, verificar contraseñas y obtener el puerto en el que el servidor debe escuchar.
+ *
+ * @author Jefer Alexis Gonzalez Romero
+ * @version 1.0 (17/03/2023)
+ */
+public class UsersService {
+
+    private static final HashMap<String, byte[]> users = new HashMap<>();
+
+    /**
+     * Método principal que agrega usuarios y configura el servidor.
+     *
+     * @param args Argumentos de la línea de comandos.
+     * @throws NoSuchAlgorithmException Si no se encuentra el algoritmo de seguridad.
+     */
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        addUser("Alexis", "123456");
+        addUser("Jefer", "654321");
+        port(getPort());
+        secure("certificados/ecikeystore.p12", "123456", null, null);
+        get("/user", (req, res) -> {
+            res.type("application/json");
+            boolean result = verifyPassword(req.queryParams("name"), req.queryParams("password"));
+            return "{\"result\":" + result + "}";
+        });
+    }
+
+    /**
+     * Hashea una contraseña utilizando el algoritmo SHA-256.
+     * Este método crea un objeto MessageDigest con el algoritmo SHA-256 y utiliza el objeto para hash la contraseña proporcionada.
+     *
+     * @param password La contraseña a hash.
+     * @return El hash de la contraseña.
+     * @throws NoSuchAlgorithmException Si no se encuentra el algoritmo de seguridad.
+     */
+    public static byte[] hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = password.getBytes();
+        return md.digest(bytes);
+    }
+
+    /**
+     * Verifica si una contraseña coincide con el hash almacenado en el mapa de usuarios.
+     * Este método obtiene el hash de la contraseña proporcionada y lo compara con el hash almacenado en el mapa de usuarios.
+     *
+     * @param userName El nombre de usuario asociado con el hash de la contraseña.
+     * @param password La contraseña a verificar.
+     * @return Verdadero si la contraseña coincide con el hash almacenado, falso en caso contrario.
+     * @throws NoSuchAlgorithmException Si no se encuentra el algoritmo de seguridad.
+     */
+    public static boolean verifyPassword(String userName, String password) throws NoSuchAlgorithmException {
+        byte[] hash = users.get(userName);
+        byte[] attemptedHash = hashPassword(password);
+        return Arrays.equals(hash, attemptedHash);
+    }
+
+    /**
+     * Obtiene el puerto en el que el servidor debe escuchar.
+     * Si la variable de entorno "PORT" está definida, se devuelve el valor de la variable de entorno como un entero.
+     * De lo contrario, se devuelve el valor predeterminado 5002.
+     *
+     * @return El puerto en el que el servidor debe escuchar.
+     */
+    public static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5002; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    /**
+     * Agrega un usuario al mapa de usuarios con su contraseña hash.
+     * Este método agrega un usuario al mapa de usuarios con su contraseña hash utilizando el método hashPassword.
+     *
+     * @param name     El nombre de usuario.
+     * @param password La contraseña del usuario.
+     * @throws NoSuchAlgorithmException Si no se encuentra el algoritmo de seguridad.
+     */
+    public static void addUser(String name, String password) throws NoSuchAlgorithmException {
+        users.put(name, hashPassword(password));
+    }
+}
